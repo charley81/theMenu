6ee2e5e856a039a5b60cc48cc9d1900bc9f30c4c
ChrisKnowsDev <chrisharley81@gmail.com>
2021-01-18 14:07:15 -0500

    created data file to hold menu items

---
+++ src/components/title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <header>
+      <h1>theMenu</h1>
+      <p>Check out our sweet interactive menu</p>
+    </header>
+  );
+};
+
+export default Title;
---
+++ src/data.js
@@ -0,0 +1,85 @@
+const menu = [
+  {
+    id: 1,
+    title: 'Bacon Plate',
+    price: '11.99',
+    category: 'breakfast',
+    img: './images/breakfast1.jpg',
+    desc:
+      "I'm baby sartorial woke 90's pok pok, post-ironic selfies migas church-key master cleanse. Taxidermy williamsburg dreamcatcher vegan aesthetic banh mi +1 art party narwhal gastropub XOXO.",
+  },
+  {
+    id: 2,
+    title: 'Noodle Salad',
+    price: '14.99',
+    category: 'lunch',
+    img: './images/lunch1.jpg',
+    desc:
+      'Church-key paleo migas glossier readymade typewriter craft beer viral hot chicken small batch art party chartreuse shabby chic. Pour-over air plant squid shaman kickstarter, vinyl mixtape before they sold out street art art party roof party.',
+  },
+  {
+    id: 3,
+    title: 'Ribeye Steak',
+    price: '24.99',
+    category: 'dinner',
+    img: './images/dinner1.jpg',
+    desc:
+      'Knausgaard humblebrag locavore actually vegan, lo-fi craft beer palo santo blog taiyaki. Letterpress small batch celiac venmo migas. Brunch vexillologist beard live-edge waistcoat locavore cronut neutra.',
+  },
+  {
+    id: 4,
+    title: 'French Toast',
+    price: '9.99',
+    category: 'breakfast',
+    img: './images/breakfast2.jpg',
+    desc:
+      'Kale chips pickled semiotics squid kitsch raclette man bun farm-to-table try-hard everyday carry jean shorts. Selfies palo santo raclette taiyaki woke. ',
+  },
+  {
+    id: 5,
+    title: 'Cheesy Soup',
+    price: '11.99',
+    category: 'lunch',
+    img: './images/lunch2.jpg',
+    desc:
+      'Vice vaporware hell of glossier put a bird on it, la croix synth jianbing palo santo skateboard bespoke +1 craft beer. Cred plaid kogi scenester adaptogen post-ironic gluten-free. ',
+  },
+  {
+    id: 6,
+    title: 'Pasta La Ham',
+    price: '14.99',
+    category: 'dinner',
+    img: './images/dinner2.jpg',
+    desc:
+      'Cliche lyft banjo crucifix waistcoat. Cray truffaut try-hard vape, hoodie succulents wayfarers copper mug PBR&B pabst cardigan polaroid.',
+  },
+  {
+    id: 7,
+    title: 'Egg & Bacon',
+    price: '7.99',
+    category: 'breakfast',
+    img: './images/breakfast3.jpg',
+    desc:
+      'Crucifix swag small batch single-origin coffee tilde jean shorts. Whatever banjo schlitz, four loko snackwave literally letterpress kickstarter taiyaki selvage ramps normcore coloring book poutine.',
+  },
+  {
+    id: 8,
+    title: 'Meatball Salad',
+    price: '12.99',
+    category: 'lunch',
+    img: './images/lunch3.jpg',
+    desc:
+      'Literally single-origin coffee meditation beard gochujang chambray heirloom hell of air plant listicle brunch lumbersexual asymmetrical bicycle rights. Hell of fanny pack +1 kale chips, bitters hella fam leggings. Disrupt keytar pour-over, iPhone prism fanny pack helvetica health goth humblebrag yr whatever four dollar toast. ',
+  },
+  {
+    id: 9,
+    title: 'Pizza',
+    price: '16.99',
+    category: 'dinner',
+    img: './images/dinner3.jpg',
+    desc:
+      "Post-ironic blue bottle woke chicharrones, asymmetrical selfies artisan. Distillery beard direct trade retro pabst, listicle farm-to-table mustache photo booth man braid 90's gluten-free actually adaptogen health goth.",
+  },
+]
+
+export default menu
